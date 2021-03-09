c36b3ab94bca064c70227daf93d127f46ee43916
Nullnes <rennasccenth@gmail.com>
2021-03-09 12:23:26 -0400

    Start project

---
+++ EF_Core_POC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EF_Core_POC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
