b2e6bcceb9972a1b7c06d79d085a2fa3b9a5a003
Nullnes <rennasccenth@gmail.com>
2021-03-10 01:17:09 -0400

    ADD items on Related Enum

--- EF_Core_POC/Models/Enums/Related.cs
+++ EF_Core_POC/Models/Enums/Related.cs
@@ -2,10 +2,12 @@ namespace EF_Core_POC.Models.Enums
 {
     public enum Related
     {
+        Outro = 0,
         Comida = 1,
         Transporte = 2,
         Mimos = 3,
         Emprestimo = 4,
-        
+        Salario = 5,
+        Investimento = 6,
     }
 }
