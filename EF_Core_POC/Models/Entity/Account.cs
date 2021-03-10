104e4519898eab466f6afa273bed8c06427a7fd1
Nullnes <rennasccenth@gmail.com>
2021-03-10 01:14:45 -0400

    Changes props to a virtual for EF Lazy Loading

--- EF_Core_POC/Models/Entity/Account.cs
+++ EF_Core_POC/Models/Entity/Account.cs
@@ -5,7 +5,7 @@ namespace EF_Core_POC.Models.Entity
     public class Account : BaseEntity
     {
         public string Owner { get; set; }
-        public IList<Transaction> Transactions { get; set; }
+        public virtual ICollection<Transaction> Transactions { get; set; }
         public long Balance { get; set; }
     }
 }
--- EF_Core_POC/Models/Entity/Transaction.cs
+++ EF_Core_POC/Models/Entity/Transaction.cs
@@ -1,11 +1,14 @@
+using System;
 using EF_Core_POC.Models.Enums;
 
 namespace EF_Core_POC.Models.Entity
 {
     public class Transaction : BaseEntity
     {
+        public Guid AccountId { get; set; }
+        public Account Account { get; set; }
         public long Amount { get; set; }
         public Related Type { get; set; }
-        public bool IsPlanned { get; set; }
+        public bool? IsPlanned { get; set; }
     }
 }
