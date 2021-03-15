87b754a599420896e56d6b6f7100da1eb79d1069
Nullnes <rennasccenth@gmail.com>
2021-03-14 22:17:21 -0400

    ADD AccountController.cs and TransactionController.cs

--- EF_Core_POC/Controllers/AccountController.cs
+++ EF_Core_POC/Controllers/AccountController.cs
@@ -1,35 +1,67 @@
+using System.Linq;
+using EF_Core_POC.Data;
+using EF_Core_POC.Settings;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Logging;
 
 namespace EF_Core_POC.Controllers
 {
+    // TODO: ADD Fluent Validation
     [Route("Account")]
-    public class AccountController : Controller
+    public class AccountController : BaseController
     {
+        #region Constructor
+
+        public AccountController(ILogger logger, RunAwayFromSerasaContext context,
+            RunAwayFromSerasaSettings settings)
+            : base(logger, context, settings)
+        {
+        }
+
+        #endregion
+
         [HttpGet]
         public IActionResult Index()
         {
-            return Ok();
+            _logger.Log(LogLevel.Trace, "Begin: Account/Index");
+
+
+            _logger.Log(LogLevel.Trace, "End: Account/Index");
+            return Ok("");
         }
-        
+
         [HttpPost]
         [Route("Create")]
         public IActionResult Create()
         {
+            _logger.Log(LogLevel.Trace, "Begin: Account/Create");
+
+
+            _logger.Log(LogLevel.Trace, "End: Account/Create");
             return Ok();
         }
-        
+
         [HttpPost]
         [Route("Update")]
         public IActionResult Update()
         {
+            _logger.Log(LogLevel.Trace, "Begin: Account/Update");
+
+
+            _logger.Log(LogLevel.Trace, "End: Account/Update");
             return Ok();
         }
-        
+
         [HttpDelete]
         [Route("Remove")]
         public IActionResult Remove()
         {
+            _logger.Log(LogLevel.Trace, "Begin: Account/Remove");
+
+
+            _logger.Log(LogLevel.Trace, "End: Account/Remove");
             return Ok();
         }
     }
--- EF_Core_POC/Controllers/TransactionController.cs
+++ EF_Core_POC/Controllers/TransactionController.cs
@@ -1,35 +1,65 @@
+using EF_Core_POC.Data;
+using EF_Core_POC.Settings;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Logging;
 
 namespace EF_Core_POC.Controllers
 {
     [Route("Transaction")]
-    public class TransactionController : Controller
+    public class TransactionController : BaseController
     {
+        #region Constructor
+
+        public TransactionController(ILogger logger, RunAwayFromSerasaContext context,
+            RunAwayFromSerasaSettings settings)
+            : base(logger, context, settings)
+        {
+        }
+
+        #endregion
+
         [HttpGet]
         public IActionResult Index()
         {
-            return Ok();
+            _logger.Log(LogLevel.Trace, "Begin: Account/Index");
+
+
+            _logger.Log(LogLevel.Trace, "End: Account/Index");
+            return Ok("");
         }
-        
+
         [HttpPost]
         [Route("Create")]
         public IActionResult Create()
         {
+            _logger.Log(LogLevel.Trace, "Begin: Account/Create");
+
+
+            _logger.Log(LogLevel.Trace, "End: Account/Create");
             return Ok();
         }
-        
+
         [HttpPost]
         [Route("Update")]
         public IActionResult Update()
         {
+            _logger.Log(LogLevel.Trace, "Begin: Account/Update");
+
+
+            _logger.Log(LogLevel.Trace, "End: Account/Update");
             return Ok();
         }
-        
+
         [HttpDelete]
         [Route("Remove")]
         public IActionResult Remove()
         {
+            _logger.Log(LogLevel.Trace, "Begin: Account/Remove");
+
+
+            _logger.Log(LogLevel.Trace, "End: Account/Remove");
             return Ok();
         }
     }
