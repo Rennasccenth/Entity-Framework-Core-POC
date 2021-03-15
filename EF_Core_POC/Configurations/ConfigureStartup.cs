2cd5b6d2fe1c29e1fde4bbdb3d4ad2280f7e2f6e
Nullnes <rennasccenth@gmail.com>
2021-03-14 23:51:36 -0400

    ADD ConfigureStartup.cs to configure IServiceCollection

---
+++ EF_Core_POC/Configurations/ConfigureStartup.cs
@@ -0,0 +1,39 @@
+using EF_Core_POC.AppService;
+using EF_Core_POC.AppService.Interfaces;
+using EF_Core_POC.Data;
+using EF_Core_POC.Settings;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EF_Core_POC.Configurations
+{
+    public static class ConfigureStartup
+    {
+        public static IServiceCollection ConfigureContextDatabase(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddDbContext<RunAwayFromSerasaContext>();
+            return serviceCollection;
+        }
+        
+        public static IServiceCollection ConfigureSingletons(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            serviceCollection.AddSingleton(new RunAwayFromSerasaSettings());
+            
+            return serviceCollection;
+        }
+        
+        public static IServiceCollection ConfigureScoped(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped<IAccountAppService, AccountAppService>();
+            serviceCollection.AddScoped<ITransactionAppService, TransactionAppService>();
+            
+            return serviceCollection;
+        }
+        
+        public static IServiceCollection ConfigureTransient(this IServiceCollection serviceCollection)
+        {
+            return serviceCollection;
+        }
+    }
+}
